d5f5c934eff963043a288642c7fbaba16a85beff
miha2158 <miha.vanya@gmail.com>
2017-01-10 19:14:33 +0500

    Lab5
    it does stuff

---
+++ Lab5/Program.cs
@@ -0,0 +1,344 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        private static readonly string[] ActionItems =
+        {
+            "",
+            "Создать новый массив",
+            "Назад"
+        };
+
+        private static readonly string[] Actions =
+        {
+            "Добавить элемент в конец массива",
+            "Удалить столбец",
+            "Добавть строки в конец массива"
+        };
+
+        #region MainInterfaceControl
+
+        private static readonly string[] StartItems =
+        {
+            "Одномерный массив",
+            "Двумерный массив",
+            "Рваный массив",
+            "Выход"
+        };
+
+        private static readonly string[] CreateArray =
+        {
+            "Заполнить вручную",
+            "Заполнить массив случайно",
+            "Назад"
+        };
+
+        public static int SelectorY(string[] items)
+        {
+            Console.CursorVisible = false;
+            int top = Console.CursorTop, left = 2;
+            int currentSelection = 0, previousSelection = 1;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.SetCursorPosition(left, top + i);
+                Console.WriteLine(items[i]);
+            }
+
+            bool selected = false;
+            do
+            {
+                #region OptionWriting
+
+                Console.SetCursorPosition(left, top + previousSelection);
+                Console.WriteLine(items[previousSelection]);
+
+                Console.SetCursorPosition(left, top + currentSelection);
+                {
+                    var temp = Console.BackgroundColor;
+                    Console.BackgroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = temp;
+                }
+                Console.WriteLine(items[currentSelection]);
+                {
+                    var temp = Console.BackgroundColor;
+                    Console.BackgroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = temp;
+                }
+
+                #endregion
+
+                previousSelection = currentSelection;
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        currentSelection++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        currentSelection--;
+                        break;
+                    case ConsoleKey.Enter:
+                        selected = true;
+                        break;
+                }
+
+                if (currentSelection < 0)
+                    currentSelection = items.Length - 1;
+                else if (currentSelection == items.Length)
+                    currentSelection = 0;
+            } while (!selected);
+
+            Console.CursorTop = top;
+            for (int i = 0; i < items.Length; i++)
+            {
+                for (int j = 0; j < Console.WindowWidth - 1; j++)
+                    Console.Write(" ");
+                Console.Write("\n");
+            }
+            Console.CursorTop -= items.Length;
+
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(items[currentSelection]);
+
+            Console.CursorTop++;
+            Console.CursorVisible = true;
+            return currentSelection;
+        }
+
+        #endregion
+
+        #region Create
+
+        public static void Create(ref int[] array, int doRandom)
+        {
+            //array = new int[0];
+            switch (doRandom)
+            {
+                case 0:
+                    Console.WriteLine("Введите элементы массива через пробел\n" +
+                                      "Будут засчитаны только числа");
+                    string input = Console.ReadLine();
+                    array = Tasks.StringToArray(input);
+                    break;
+
+                case 1:
+                    Console.WriteLine("Введите длину массива");
+                    int length;
+                    while (!int.TryParse(Console.ReadLine(), out length) || length >= 128)
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("Длинна должна быть целым числом меньше 128");
+                    }
+                    array = new int[length];
+                    Tasks.Randomise(ref array);
+                    break;
+            }
+            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(array));
+        }
+
+        public static void Create(ref int[][] array, int doRandom)
+        {
+            //array = new int[0][];
+            switch (doRandom)
+            {
+                case 0:
+                    Console.WriteLine("Введите элементы массива через пробел \nБудут засчитаны только числа");
+                    string[] input = new string[1];
+                    for (int i = 0;; i++)
+                    {
+                        input[i] = Console.ReadLine();
+                        if (input[i] == "")
+                            break;
+
+                        string[] temp = new string[input.Length + 1];
+                        input.CopyTo(temp, 0);
+                        input = temp;
+                    }
+                    array = Tasks.StringToArray(input);
+                    break;
+
+                case 1:
+                    Console.WriteLine("Введите количество строк массива");
+                    int length;
+                    while (!int.TryParse(Console.ReadLine(), out length) || length >= 128)
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("Длинна должна быть целым числом меньше 128");
+                    }
+                    array = new int[length][];
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.WriteLine("Введите длину {0} строки", i + 1);
+                        int width;
+                        while (!int.TryParse(Console.ReadLine(), out width) || width > 128)
+                        {
+                            Console.CursorTop--;
+                            Console.WriteLine("Длинна должна быть целым числом меньше 128");
+                        }
+                        array[i] = new int[width];
+                    }
+                    Tasks.Randomise(ref array);
+                    break;
+            }
+            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(array));
+        }
+
+        public static void Create(ref int[,] array, int doRandom)
+        {
+            //array = new int[0, 0];
+            switch (doRandom)
+            {
+                case 0:
+                    Console.WriteLine("Введите элементы массива через пробел\n" +
+                                      "Будут засчитаны только числа");
+                    string[] input = new string[1];
+                    for (int i = 0;; i++)
+                    {
+                        input[i] = Console.ReadLine();
+                        if (input[i] == "")
+                            break;
+
+                        string[] temp = new string[input.Length + 1];
+                        input.CopyTo(temp, 0);
+                        input = temp;
+
+                    }
+                    array = Tasks.JaggedTo2D(Tasks.StringToArray(input));
+                    break;
+
+                case 1:
+                    Console.WriteLine("Введите количество строк массива");
+                    int length;
+                    while (!int.TryParse(Console.ReadLine(), out length) || length >= 128)
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("Длинна должна быть целым числом меньше 128");
+                    }
+                    Console.WriteLine("Введите количество столбцов массива");
+                    int width;
+                    while (!int.TryParse(Console.ReadLine(), out width) || width >= 128)
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("Длинна должна быть целым числом меньше 128");
+                    }
+                    array = new int[length, width];
+                    Tasks.Randomise(ref array);
+                    break;
+            }
+            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(array));
+        }
+
+        #endregion
+        
+        public static void Action(int index, int arrayType, ref int[] ar1D, ref int[,] ar2D, ref int[][] arJ)
+        {
+            switch (index)
+            {
+                case 0:
+                    int temp;
+
+                    switch (arrayType)
+                    {
+                        case 0:
+                            Console.WriteLine("Введите новый элемент");
+                            while (!int.TryParse(Console.ReadLine(), out temp))
+                            {
+                                Console.CursorTop--;
+                                Console.WriteLine("Новый элемент должен быть целым числом меньше  {0}", int.MaxValue);
+                            }
+                            ar1D = Tasks.Extend1Dim(ar1D, temp);
+                            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(ar1D));
+                            break;
+
+                        case 1:
+                            if (ar2D == null || ar2D.Length == 0)
+                            {
+                                Console.WriteLine("Нельзя удалить столбец из пустого массива\n");
+                                break;
+                            }
+                            Console.WriteLine("Введите номер лишнго столбца");
+                            while (!int.TryParse(Console.ReadLine(), out temp) && temp > 0 && temp < ar2D.GetLength(1))
+                            {
+                                Console.CursorTop--;
+                                Console.WriteLine("Номер столбца должен быть целым числом от 1 до {0}", ar2D.GetLength(1));
+                            }
+
+                            temp--;
+                            ar2D = Tasks.Shrink2Dim(ar2D, temp);
+                            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(ar2D));
+                            break;
+
+                        case 2:
+                            int[][] newStuff = null;
+                            Console.WriteLine("Как получить новые строки?");
+                            temp = SelectorY(CreateArray);
+                            Create(ref newStuff, temp);
+
+                            arJ = Tasks.ExtendJagged(arJ, newStuff);
+                            Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(arJ));
+                            break;
+                    }
+                    break;
+
+                case 1:
+                    int action = SelectorY(CreateArray);
+                    switch (arrayType)
+                    {
+                        case 0:
+                            Create(ref ar1D, action);
+                            break;
+                        case 1:
+                            Create(ref ar2D, action);
+                            break;
+                        case 2:
+                            Create(ref arJ, action);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] ar1D = {0};
+            int[,] ar2D = {{0}};
+            int[][] arJ = new int[1][];
+            arJ[0] = new[] {0};
+            Console.WriteLine(" ");
+            do
+            {
+                int index = SelectorY(StartItems);
+                if (index == 3)
+                    break;
+
+                ActionItems[0] = Actions[index];
+                int action = 1;
+
+                switch (index)
+                {
+                    case 0:
+                        Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(ar1D));
+                        break;
+                    case 1:
+                        Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(ar2D));
+                        break;
+                    case 2:
+                        Console.WriteLine("\nВаш массив: \n{0}", Tasks.ArrayToString(arJ));
+                        break;
+                }
+
+                do
+                {
+                    action = SelectorY(ActionItems);
+                    Action(action, index, ref ar1D, ref ar2D, ref arJ);
+                } while (action != 2);
+
+                ActionItems[0] = "";
+
+            } while (true);
+        }
+    }
+}
---
+++ Lab5/Tasks.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab5
+{
+    public class Tasks
+    {
+        #region tasks
+
+        public static int[][] ExtendJagged(int[][] array, int[][] newStuff)
+        {
+            if (array == null || array.Length == 0)
+                return newStuff;
+            if (newStuff == null || newStuff.Length == 0)
+                return array;
+            
+            int[][] temp = new int[array.Length + newStuff.Length][];
+
+            for (int i = 0; i < array.Length; i++)
+                temp[i] = array[i];
+            for (int i = 0; i < newStuff.Length; i++)
+                temp[i + array.Length] = newStuff[i];
+
+            return temp;
+        }
+
+        public static int[] Extend1Dim(int[] array, int element)
+        {
+            int[] temp = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+                temp[i] = array[i];
+            temp[temp.Length - 1] = element;
+
+            return temp;
+        }
+
+        public static int[,] Shrink2Dim(int[,] array, int column)
+        {
+            if (array == null)
+                return null;
+
+            int l0 = array.GetLength(0);
+            int l1 = array.GetLength(1) - 1;
+
+            int[,] temp = new int[l0, l1];
+
+            for (int i0 = 0; i0 < l0; i0++)
+                for (int i1 = 0, j1 = 0; i1 < l1; i1++, j1++)
+                {
+                    if (i1 == column)
+                        j1++;
+                    temp[i0, i1] = array[i0, j1];
+                }
+
+            return temp;
+        }
+
+        #endregion
+
+        #region randomise
+
+        static readonly Random Rand = new Random();
+        private const int RMin = -99;
+        private const int RMax = 100;
+        private const int ReasonableSize = 10;
+
+        public static void Randomise(ref int[] array)
+        {
+            if (array == null)
+                array = new int[Rand.Next(ReasonableSize) + 1];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = Rand.Next(RMin, RMax);
+        }
+
+        public static void Randomise(ref int[,] array)
+        {
+            if (array == null)
+                array = new int[Rand.Next(ReasonableSize) + 1, Rand.Next(ReasonableSize) + 1];
+
+            int l1 = array.GetLength(0);
+            int l2 = array.GetLength(1);
+
+            for (int i0 = 0; i0 < l1; i0++)
+                for (int i1 = 0; i1 < l2; i1++)
+                    array[i0, i1] = Rand.Next(RMin, RMax);
+        }
+
+        public static void Randomise(ref int[][] array)
+        {
+            if (array == null)
+                array = new int[Rand.Next(ReasonableSize) + 1][];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Randomise(ref array[i]);
+            }
+        }
+
+        #endregion
+
+        #region input
+
+        public static int[] StringToArray(string input)
+        {
+            int[] result = new int[0];
+            input = input.Replace("  ", " ");
+
+            string[] temp = input.Split(' ');
+
+            for (int i = 0,j = 0; i < temp.Length; i++)
+            {
+                int temp1;
+                if (temp[i].Trim() != string.Empty && int.TryParse(temp[i], out temp1))
+                {
+                    int[] array = new int[result.Length + 1];
+                    result.CopyTo(array, 0);
+                    array[j++] = temp1;
+                    result = array;
+                }
+
+            }
+            return result;
+        }
+
+        public static int[][] StringToArray(string[] input)
+        {
+            int[][] array = new int[0][];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != string.Empty)
+                {
+                    int[][] temp = new int[array.Length + 1][];
+                    array.CopyTo(temp, 0);
+                    temp[i] = StringToArray(input[i]);
+
+                    if(temp[i].Length == 0)
+                        temp[i] = null;
+
+                    array = temp;
+                }
+            }
+
+
+            if (array.Length == 0)
+                return null;
+            if (array.All(i => i == null || i.Length == 0))
+                return null;
+
+            return array;
+        }
+
+        public static int[,] JaggedTo2D(int[][] array)
+        {
+            int width = array[0].Length;
+            for (int i = 1; i < array.Length; i++)
+                if (array[i].Length > width)
+                    width = array[i].Length;
+
+            int[,] result = new int[array.Length, width];
+            for (int i0 = 0; i0 < array.Length; i0++)
+                for (int i1 = 0; i1 < width; i1++)
+                    try
+                    {
+                        result[i0, i1] = array[i0][i1];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        result[i0, i1] = 0;
+                    }
+            return result;
+        }
+
+        #endregion
+
+        #region output
+
+        public const string OutputFormat = "{0} {1,4}";
+
+        public static string ArrayToString(int[] array)
+        {
+            string output = String.Empty;
+            if (array == null || array.Length == 0)
+                output = " Пустой массив";
+            else
+                foreach (int item in array)
+                    output = String.Format(OutputFormat, output, item);
+            output = output + "\n";
+
+            return output;
+        }
+
+        public static string ArrayToString(int[,] array)
+        {
+            string output = String.Empty;
+
+            if (array == null || array.GetLength(0) == 0 || array.GetLength(1) == 0)
+                output = " Пустой Массив\n";
+            else
+                for (int i0 = 0; i0 < array.GetLength(0); i0++)
+                {
+                    for (int i1 = 0; i1 < array.GetLength(1); i1++)
+                        output = String.Format(OutputFormat, output, array[i0, i1]);
+                    output = output + "\n";
+                }
+
+            return output;
+        }
+
+        public static string ArrayToString(int[][] array)
+        {
+            string output = String.Empty;
+
+            if (array == null || array.Length == 0)
+                output = " Пустой массив\n";
+            else
+                foreach (var item in array)
+                {
+                    output = output + ArrayToString(item);
+                }
+            return output;
+        }
+
+        #endregion
+
+    }
+}
